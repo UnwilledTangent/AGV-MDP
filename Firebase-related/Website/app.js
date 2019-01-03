34d2d7f7cb9ddbc8bffbb9e263c2305a744c1696
Sean Tin <tinsean@hotmail.com>
2019-01-03 17:19:45 +0800

    added feature: listen to loaded program and program state

--- Firebase-related/Website/app.js
+++ Firebase-related/Website/app.js
@@ -4,11 +4,15 @@
 //	});
 //});
 
-const displayDirection = document.querySelector('#direction');
+const displayUR5Info = document.querySelector('#URinfo');
 let directionHeader = document.createElement('h2');
+let loadedProgramHeader = document.createElement('h2');
+let programStateHeader = document.createElement('h2');
 
 
-var docRef = db.collection("UR5").doc("position");
+var positionDocRef = db.collection("UR5").doc("position");
+var loadedProgramDocRef = db.collection("UR5").doc("loadedProgram");
+var programStateDocRef = db.collection("UR5").doc("programState");
 
 // Reads the Firebase server once
 //docRef.get().then(function (doc) {
@@ -22,8 +26,20 @@ var docRef = db.collection("UR5").doc("position");
 //})
 
 // Listens to the Firebase server constantly for updates
-db.collection("UR5").doc("position")
+positionDocRef
 	.onSnapshot(function (doc) {
 		directionHeader.textContent = "UR5 is facing: " + doc.data().direction;
-		displayDirection.appendChild(directionHeader);
+		displayUR5Info.appendChild(directionHeader);
+	});
+
+loadedProgramDocRef
+	.onSnapshot(function (doc) {
+		loadedProgramHeader.textContent = doc.data().loadedProgram;
+		displayUR5Info.appendChild(loadedProgramHeader);
+	});
+
+programStateDocRef
+	.onSnapshot(function (doc) {
+		programStateHeader.textContent = doc.data().programState;
+		displayUR5Info.appendChild(programStateHeader);
 	});
